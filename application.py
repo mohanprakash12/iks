0d6c90a28c38e37c669469ec875eb7709e4035da
Mohan Prakash <102145704+mohanprakash12@users.noreply.github.com>
2022-06-10 09:55:54 +0100

    Update application.py

--- application.py
+++ application.py
@@ -49,7 +49,7 @@ if uploaded_file:
 st.subheader('sales')
 
 hist_values = np.histogram(
-    data[sales].dt.hour, bins=24, range=(0,24))[0]
+    data[InvNeSUM].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
