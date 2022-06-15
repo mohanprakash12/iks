13764ffb7e0fd41acb3c3fdcbb2860192c78997a
Mohan Prakash <102145704+mohanprakash12@users.noreply.github.com>
2022-06-15 14:10:34 +0100

    Update application.py

--- application.py
+++ application.py
@@ -23,43 +23,37 @@ if add_selectbox == "Online":
         MemberType = st.selectbox('Member Type:', ('Full', 'Associate','2 nd Account', 'other MemberType'))
   
 else:
-  uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
-    input_df = pd.read_csv(uploaded_file)
-    st.write(
-    '''
-    ### Input Data ({} Customers)
-    '''.format(input_df.shape[0])
-    )
-    st.dataframe(input_df)
-    st.write('')
-    rfm = pickle.load( open( "modelsy.pkl", "rb" ) )
+        uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+        input_df = pd.read_csv(uploaded_file)
+        st.write(
+                '''
+                ### Input Data ({} Customers)
+                '''.format(input_df.shape[0])
+        )
+        st.dataframe(input_df)
+        st.write('')
+        rfm = pickle.load( open( "modelsy.pkl", "rb" ) )
 
-    X = input_df.drop(labels = ['CMemNo'], axis = 1)
+        X = input_df.drop(labels = ['CMemNo'], axis = 1)
 
-    threshold = .22
-    y_preds = rfm.predict(X)
-    predicted_proba = rfm.predict_proba(X)
-    y_preds = (predicted_proba [:,1] >= threshold).astype('int')
-    op_list = []
-    for idx, Failed in enumerate(y_preds):
-        if Failed == 0:
-            op_list.append(input_df.CMemNo.iloc[idx])
-    st.write('''### Number of Potentially Churning Customers Payment''')
-    st.write('''There are **{} customers** at risk of Payment Failure.'''.format(len(op_list)))
-
-    csv = pd.DataFrame(op_list).to_csv(index=False, header = False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    st.write('''''')
-    st.write('''''')
-    st.write('''### **⬇️ Download At-Risk CMember Id's**''')
-    href = f'<a href="data:file/csv;base64,{b64}" download="at_risk_customerids.csv">Download csv file</a>'
-    st.write(href, unsafe_allow_html=True)
-    
-
-
-
-
-# In[ ]:
+        threshold = .22
+        y_preds = rfm.predict(X)
+        predicted_proba = rfm.predict_proba(X)
+        y_preds = (predicted_proba [:,1] >= threshold).astype('int')
+        op_list = []
+        for idx, Failed in enumerate(y_preds):
+                if Failed == 0:
+                        op_list.append(input_df.CMemNo.iloc[idx])
+                        st.write('''### Number of Potentially Churning Customers Payment''')
+                        st.write('''There are **{} customers** at risk of Payment Failure.'''.format(len(op_list)))
+                        csv = pd.DataFrame(op_list).to_csv(index=False, header = False)
+                        b64 = base64.b64encode(csv.encode()).decode()
+                        st.write('''''')
+                        st.write('''''')
+                        st.write('''### **⬇️ Download At-Risk CMember Id's**''')
+                        href = f'<a href="data:file/csv;base64,{b64}" download="at_risk_customerids.csv">Download csv file</a>'
+                        st.write(href, unsafe_allow_html=True)
+                        # In[ ]:
 
 
 
