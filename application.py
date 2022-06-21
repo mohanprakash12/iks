b7d15567d3d5dc91e93b162a71bbe9071cd74a6c
Mohan Prakash <102145704+mohanprakash12@users.noreply.github.com>
2022-06-21 11:12:11 +0100

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 model = pickle.load( open( "rand_forest_mo.p", "rb" ) )
 
 def predict(model, input_df):
