c953d329e49299c44266dae2ca7ae3ad34dc5070
Mohan Prakash <102145704+mohanprakash12@users.noreply.github.com>
2022-06-21 10:51:39 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,51 +1,47 @@
 from pycaret.classification import load_model, predict_model
+import numpy as np
 import streamlit as st
 import pandas as pd
-import numpy as np
-model = pickle.load( open( "modelsly.pkl", "rb" ) )
+import matplotlib.pyplot as plt
+import pickle
+import base64
+from sklearn.preprocessing import StandardScaler
 
-def predict(model, input_df):
-	predictions_df = predict_model(estimator=model, data=input_df)
-	predictions = predictions_df['Label'][0]
-	return predictions
+st.write('''# *Customer Payment Predictor*''')
+  
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+
+if uploaded_file:
+    input_df = pd.read_csv(uploaded_file)
+    st.write(
+    '''
+    ### Input Data ({} Customers)
+    '''.format(input_df.shape[0])
+    )
+    st.dataframe(input_df)
+    st.write('')
+    rfm = pickle.load( open( "modelsly", "rb" ) )
+
+    X = input_df.drop(labels = ['CMemNo'], axis = 1)
+
+    threshold = .22
+    y_preds = rfm.predict(X)
+    predicted_proba = rfm.predict_proba(X)
+    y_preds = (predicted_proba [:,1] >= threshold).astype('int')
+    op_list = []
+    for idx, Failed in enumerate(y_preds):
+        if Failed == 0:
+            op_list.append(input_df.CMemNo.iloc[idx])
+    st.write('''### Number of Potentially Churning Customers Payment''')
+    st.write('''There are **{} customers** at risk of Payment Failure.'''.format(len(op_list)))
+
+    csv = pd.DataFrame(op_list).to_csv(index=False, header = False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    st.write('''''')
+    st.write('''''')
+    st.write('''### **⬇️ Download At-Risk CMember Id's**''')
+    href = f'<a href="data:file/csv;base64,{b64}" download="at_risk_customerids.csv">Download csv file</a>'
+    st.write(href, unsafe_allow_html=True)
 
 
-def main():
-	add_selectbox = st.sidebar.selectbox(
-	"How would you like to predict?",
-	("Online", "Batch"))
-	st.sidebar.info('This app is created to predict Customer payment failure')
-	st.title("Predicting Customer payment Failure")
-	if add_selectbox == 'Online':
-		sales = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=110000, value=0)
-		MemberType = st.selectbox('Member Type:', ('Full', 'Other MemberType', 'Associate', '2nd Account'))
-		ProductCategory = st.selectbox('ProductCategory:', ('Fixed & Broadband', 'Liquid', 'Agrochemicals', 'Other products','Charge card', 'General - Machinery', 'General - Supplies',
-                     'Building Materials', 'Mobile', 'Non HH, mains gas, MOP','Fuel Cards'))
-
-		Town = st.selectbox('Town:', ('Thetford', 'Holt', 'Wisbech', 'Other Area', 'Norwich', 'Dereham','Kings Lynn', 'Lowestoft', 'Huntingdon', 'Diss', 
-					      'Beccles','Fakenham', 'Spalding', 'Bury St Edmunds', 'Colchester','Gt Yarmouth', 'Bungay', 'North Walsham', 'Wymondham',
-					      'Great Yarmouth', 'Cambridge', 'Peterborough', 
-					      'Ely',"King's Lynn", 'Attleborough', 'March', 'Swaffham','Downham Market'))
-		yearofjoning = st.number_input('Member Joining Year',min_value=0, max_value=3000, value=0)
-		monthofjoining = st.number_input('Member Joining Month',min_value=0, max_value=12, value=0)
-		dayofjoining = st.number_input('Member Joining Day',min_value=0, max_value=31, value=0)
-		Zerosales=st.slider('is the invoice amount is zero if yes 1' , min_value=0, max_value=1, value=1)
-		refunded=st.slider('is the invoice amount is in -values if yes press 1' , min_value=0, max_value=1, value=1)
-		
-		output=""
-		input_dict={'sales':sales,'MemberType':MemberType,'ProductCategory':ProductCategory,'Town':Town,'yearofjoning':yearofjoning
-		,'monthofjoining':monthofjoining,'dayofjoining':dayofjoining,'Zerosales':Zerosales
-		,'refunded':refunded}
-		input_df = pd.DataFrame([input_dict])
-		if st.button("Predict"):
-			output = predict(model=model, input_df=input_df)
-			output = str(output)
-		st.success('Churn : {}'.format(output))
-	if add_selectbox == 'Batch':
-		file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
-		if file_upload is not None:
-			data = pd.read_csv(file_upload)
-			predictions = predict_model(estimator=model,data=data)
-			st.write(predictions)
-if __name__ == '__main__':
-	main()
+# In[ ]:
